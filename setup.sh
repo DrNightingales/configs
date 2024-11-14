05f02dd490020c640e3637cd6ea953c62f6e4dd0
Peter Solovyev <petersoloviev123@gmail.com>
2024-11-14 14:39:19 +0300

    config update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
 cp ./.vimrc ~
 cp ./.zshrc ~
