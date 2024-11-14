f6d926b251f3e41f572ff30f6e10b6905a15280d
Peter Solovyev <petersoloviev123@gmail.com>
2024-11-14 14:45:26 +0300

    split configs

--- 2_install_zsh_autosuggestions.sh
+++ 2_install_zsh_autosuggestions.sh
@@ -1,4 +1,1 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
-cp ./.vimrc ~
-cp ./.zshrc ~
